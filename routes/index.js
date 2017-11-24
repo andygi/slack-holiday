976a2617622021b5bd26e022fb5438258b8feb8e
Andrea Greco <andrea.greco@kingfisher.com>
2017-11-24 12:39:57 +0000

    update date formatting

--- routes/index.js
+++ routes/index.js
@@ -1,45 +1,45 @@
-'use strict'
+"use strict"
 
-const express = require('express'),
+const express = require("express"),
   router = express.Router(),
-  root = process.cwd() + '/',
-  fs = require('fs'),
-  config = require(root + 'config.json'),
-  deviceList = require(root + 'assets/devices.json')
+  root = process.cwd() + "/",
+  fs = require("fs"),
+  config = require(root + "config.json"),
+  deviceList = require(root + "assets/devices.json")
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', {
-    title: 'Express'
+router.get("/", (req, res, next) => {
+  res.render("index", {
+    title: "Express"
   })
 })
 
 // devices route
-router.post('/holiday', (req, res, next) => {
+router.post("/holiday", (req, res, next) => {
   if (!req.body || req.body.token !== config.verificationToken) {
     res.status(400).send({
-      test: 'Service temporarily unavailable'
+      test: "Service temporarily unavailable"
     })
   } else {
     let resp
     switch (req.body.text) {
-      case '?':
-        resp = '*please declare your holiday days*'
-        break
-
+      case "?":
+        resp = "*please declare your holiday days*"
+        break;
       default:
-        resp = createHolidayRequest(req.body.text)
+        resp = createHolidayRequest(req.body.text);
+        break;
     }
     res.send(resp)
   }
 })
 
 // This route will answer button clicks
-router.post('/buttons', (req, res, next) => {
+router.post("/buttons", (req, res, next) => {
   let payload = JSON.parse(req.body.payload)
   if (!payload || payload.token !== config.verificationToken) {
     res.status(400).send({
-      test: 'Service temporarily unavailable'
+      test: "Service temporarily unavailable"
     })
   } else {
     let device = deviceList[payload.actions[0].value],
@@ -49,13 +49,10 @@ router.post('/buttons', (req, res, next) => {
       }
 
     switch (action) {
-      case 'send_request':
-      console.log(payload.actions.value);
-      console.log("___________IN");
+      case "send_request":
         console.log(payload);
         resp.text = `The request is sent`;
         resp.delete_original = true;
-
         break;
       default:
         console.log("default");
@@ -68,34 +65,52 @@ router.post('/buttons', (req, res, next) => {
 module.exports = router
 
 // private functions
+function convertDate(value) {
+  let output = 0;
+  if (value) {
+    let valueAr = value.split("/");
+    output = new Date(valueAr[2], valueAr[1] - 1, valueAr[0]).getTime() / 1000;
+  } else {
+    output = Math.round(new Date().getTime() / 1000);
+  }
+  return output;
+}
+
+function unconvertDate(value) {
+  let myDate = new Date(value*1000);
+  let output = myDate.getDate() + "/" + (myDate.getMonth() + 1) + "/" + myDate.getFullYear();
+  return output;
+}
+
 function createHolidayRequest(payload) {
   let dates = payload.split(" ");
-  let dateFrom = dates[0];
-  let dateTo = dates[1];
+  let today = convertDate();
+  let dateFrom = convertDate(dates[0]);
+  let dateTo = convertDate(dates[1]);
   let response = {
       mrkdwn: true,
-      text: 'Here your *holiday request*.\nAfter booking your request will be submited to your line manager for approval.',
+      text: "Here your *holiday request*.\nAfter booking your request will be submited to your line manager for approval.",
       attachments: [
         {
-          color: '#000000',
+          color: "#000000",
           mrkdwn_in: ["fields"],
           callback_id: "holiday_request",
           fields: [
             {
-              title: 'From',
+              title: "From",
               short: true
             },
             {
-              title: 'To',
+              title: "To",
               short: true
             }
           ],
           actions: [
             {
-              name: 'send_request',
-              text: 'Submit the request',
-              type: 'button',
-              style: 'primary',
+              name: "send_request",
+              text: "Submit the request",
+              type: "button",
+              style: "primary",
               value: dateFrom + ";" + dateTo
             }
           ]
@@ -103,7 +118,7 @@ function createHolidayRequest(payload) {
       ]
     }
 
-  response.attachments[0].fields[0].value = dateFrom;
-  response.attachments[0].fields[1].value = dateTo;
+  response.attachments[0].fields[0].value = unconvertDate(dateFrom);
+  response.attachments[0].fields[1].value = unconvertDate(dateTo);
   return response
 }
